2824b9946c85a1b85a49fde8cbbf15c286caa571
Elfin <mr.hawl1227@gmail.com>
2022-11-19 13:58:53 +0300

    111920221359P

---
+++ ReverseAndNot/ReverseAndNot.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseAndNot
+{
+    public class ReverseAndNot
+    {
+        public static string ReverseAndPlace(string text)
+        {
+            string reverseText = "";
+
+            for (int i = (text.Length - 1); i >= 0; i--)
+            {
+                reverseText += text[i];
+            }
+
+            return text + reverseText;
+        }
+    }
+}
